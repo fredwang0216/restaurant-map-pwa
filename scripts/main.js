8d4760196f6920af6e4dc25da36bb893c53dbae1
zhuyezi531 <162005071+zhuyezi531@users.noreply.github.com>
2024-06-09 22:12:27 +0800

    "Add filter functionality"

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,54 @@
+let map;
+let markers = [];
+let locations = [];
 
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: { lat: 1.290270, lng: 103.851959 } // Default to Singapore center
     });
 
     fetch('locations.json')
         .then(response => response.json())
-        .then(locations => {
-            locations.forEach(location => {
-                const marker = new google.maps.Marker({
-                    position: location.geometry.location,
-                    map: map,
-                    title: location.name
-                });
-
-                const infowindow = new google.maps.InfoWindow({
-                    content: `<h3>${location.name}</h3><p>${location.formatted_address}</p><p>Rating: ${location.rating}</p>`
-                });
-
-                marker.addListener('click', () => {
-                    infowindow.open(map, marker);
-                });
-            });
+        .then(data => {
+            locations = data;
+            addMarkers('Asian'); // Default filter
         })
         .catch(error => console.error('Error fetching locations:', error));
+
+    document.querySelectorAll('.filter-btn').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const type = btn.getAttribute('data-type');
+            addMarkers(type);
+        });
+    });
+}
+
+function addMarkers(type) {
+    // Remove existing markers
+    markers.forEach(marker => marker.setMap(null));
+    markers = [];
+
+    // Add new markers based on filter
+    locations.forEach(location => {
+        if (type === 'All' || location.type === type) {
+            const marker = new google.maps.Marker({
+                position: location.geometry.location,
+                map: map,
+                title: location.name
+            });
+
+            const infowindow = new google.maps.InfoWindow({
+                content: `<h3>${location.name}</h3><p>${location.formatted_address}</p><p>Rating: ${location.rating}</p>`
+            });
+
+            marker.addListener('click', () => {
+                infowindow.open(map, marker);
+            });
+
+            markers.push(marker);
+        }
+    });
 }
 
 // Register service worker
